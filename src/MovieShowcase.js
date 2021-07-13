1dc4311322c28a7d22a8ff12d363ce7f2762fa55
SET mentor <setmentor_parttime02@academyxi.com>
2021-07-13 21:17:49 +1000

    done

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,9 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(data => {
+      return <MovieCard key={data.title} title={data.title} genres={data.genres} poster={data.poster} IMDBRating={data.IMDBRating} />
+    })
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MovieShowcase from './MovieShowcase';
+import movieData from './data.js'
 
 ReactDOM.render(<MovieShowcase />, document.getElementById('root'));
